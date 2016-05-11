59e2cb4c37ebac86502cfa63a974b1dbc407e4f9
Sandro Mancuso <sandromancuso@yahoo.com>
2016-05-11 16:39:16 +0100

    Working solution

--- src/main/scala/com/codurance/bowlingkata/BowlingScoreCalculator.scala
+++ src/main/scala/com/codurance/bowlingkata/BowlingScoreCalculator.scala
@@ -3,27 +3,45 @@ package com.codurance.bowlingkata
 object BowlingScoreCalculator {
 
 	def scoreFor(rolls: String): Int =
-		calculateScore(toPins(rolls.split("")))
+		totalScore(rolls.split("").toList)
 
-	private def toPins(rolls: Array[String], pins: Array[Int] = Array()): Array[Int] =
-		rolls match {
-			case Array() => pins
-			case Array("-", _*) => toPins(rolls.tail, pins :+ 0)
-			case Array("/", _*) => toPins(rolls.tail, pins :+ 10 - pins.last)
-			case Array(c, _*) => toPins(rolls.tail, pins :+ c.toInt)
+	private def totalScore(rolls: List[String], index: Int = 0, score: Int = 0): Int =
+		rolls.drop(index) match {
+			case List(STRIKE._1, _*) => totalScore(rolls, index + 1, score + strike(rolls, index))
+			case List(SPARE, _*) => totalScore(rolls, index + 1, score + spare(rolls, index))
+			case List(MISS._1, _*) => totalScore(rolls, index + 1, score)
+			case List(s, _*) => totalScore(rolls, index + 1, score + s.toInt)
+			case List() => score
 		}
 
-	private def calculateScore(pins: Array[Int]): Int = {
-		var score = 0
-		for (i <- pins.indices) {
-			score += pins(i)
-		     if (i % 2 == 0) {
-			    if (pins(i - 1) + pins(i) == 10) {
-					score += (10 + pins.applyOrElse(i + 1, _ => 0))
-				}
-			}
-		}
-		score
-	}
+	private def strike(rolls: List[String], index: Int): Int =
+		if (numberOfPreviousStrikes(rolls, index) + index < TENTH_FRAME_FIRST_ROLL)
+	 		 10 + bonusRoll(rolls, index + 1) + bonusRoll(rolls, index + 2)
+		else 10
 
+	private def numberOfPreviousStrikes(rolls: List[String], index: Int): Int = rolls.mkString.take(index).count(_ == STRIKE._1.head)
+
+	private def spare(rolls: List[String], index: Int): Int =
+		if (index < TENTH_FRAME_SECOND_ROLL)
+			 spareValue(rolls, index) + bonusRoll(rolls, index + 1)
+		else spareValue(rolls, index)
+
+	private def spareValue(rolls: List[String], index: Int): Int = 10 - rolls(index - 1).toInt
+
+	private def bonusRoll(rolls: List[String], index: Int): Int =
+		value(rolls.lift(index).getOrElse("0")).toInt
+
+	private def value(s: String): String =
+		s match {
+			case MISS._1 => MISS._2
+			case STRIKE._1 => STRIKE._2
+			case _ => s
+		}
+
+	val MISS = ("-", "0")
+	val STRIKE = ("X", "10")
+	val SPARE = "/"
+	val MAX_ROLLS = 21
+	val TENTH_FRAME_FIRST_ROLL = MAX_ROLLS - 3
+	val TENTH_FRAME_SECOND_ROLL = MAX_ROLLS - 2
 }
--- src/test/scala/com/codurance/bowlingkata/BowlingScoreCalculatorShould.scala
+++ src/test/scala/com/codurance/bowlingkata/BowlingScoreCalculatorShould.scala
@@ -9,8 +9,9 @@ import org.scalatest.junit.JUnitRunner
 class BowlingScoreCalculatorShould extends UnitSpec {
 
 	"calculate scores with no strikes or spares" in {
+		scoreFor("1") should be (1)
 		scoreFor("13") should be (4)
-		scoreFor("135213") should be (15)
+		scoreFor("13521") should be (12)
 	}
 
 	"calculate scores containing a miss" in {
@@ -19,20 +20,20 @@ class BowlingScoreCalculatorShould extends UnitSpec {
 	}
 
 	"calculate scores containing spares" in {
-//		scoreFor("1/") should be (10)
+		scoreFor("1/") should be (10)
 		scoreFor("1/--") should be (10)
 		scoreFor("1/-5") should be (15)
 		scoreFor("1/35") should be (21)
-//		scoreFor("1/3/23") should be (30)
-//		scoreFor("5/5/5/5/5/5/5/5/5/5/5") should be (150)
+		scoreFor("1/3/23") should be (30)
+		scoreFor("5/5/5/5/5/5/5/5/5/5/5") should be (150)
 	}
 
 	"calculate scores containing strikes" in {
-//		scoreFor("X") should be (10)
-//		scoreFor("X--") should be (10)
-//		scoreFor("X--51") should be (16)
-//		scoreFor("X51") should be (16)
-//		scoreFor("XXXXXXXXXXXX") should be(300)
-//		scoreFor("XXXXXXXXXX12") should be(274)
+		scoreFor("X") should be (10)
+		scoreFor("X--") should be (10)
+		scoreFor("X--51") should be (16)
+		scoreFor("X51") should be (22)
+		scoreFor("XXXXXXXXXXXX") should be(300)
+		scoreFor("XXXXXXXXXX12") should be(274)
 	}
 }
