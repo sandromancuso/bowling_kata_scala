33b9091a88e80bdab96c5c1ea4aa63a6eed95572
Sandro Mancuso <sandromancuso@yahoo.com>
2016-05-13 12:04:10 +0200

    Refactored partial score solution

--- src/main/scala/com/codurance/bowlingkata/partial_scoring/BowlingPartialScoreCalculator.scala
+++ src/main/scala/com/codurance/bowlingkata/partial_scoring/BowlingPartialScoreCalculator.scala
@@ -5,44 +5,30 @@ object BowlingPartialScoreCalculator {
 	def scoreFor(rolls: String): Int = totalScore(rolls.split("").toList)
 
 	private def totalScore(rolls: List[String], index: Int = 0, score: Int = 0): Int = {
+		lazy val MISS = "-"
+		lazy val SPARE = ("/", () => 10 - rollScoreAt(index - 1) + if_(index < 19, rollScoreAt(index + 1)))
+		lazy val STRIKE = ("X", () => 10 + if_(index + numberOfPreviousStrikes() < 18,
+												rollScoreAt(index + 1) + rollScoreAt(index + 2)))
 
 		def numberOfPreviousStrikes(): Int = rolls.mkString.take(index).count(_ == 'X')
 
-		lazy val STRIKE = ("X", strikeScore)
-		lazy val SPARE = ("/", spareScore)
-		lazy val MISS = "-"
-		lazy val ROLL = "r"
-		lazy val NONE = ""
-		def roll(i: Int): Int = rollScoreAt(rolls.drop(i).headOption)._2
-		def previousRoll(): Int = rollScoreAt(rolls.take(index).lastOption)._2
-
-		def isSpare(i: Int): Boolean = rollScoreAt(rolls.drop(i).headOption)._1 == SPARE
-
-		def spareScore() = 10 - previousRoll() + if_(index + numberOfPreviousStrikes < 19, roll(index + 1))
-
-		def strikeScore() =
-			10 + if_(index + numberOfPreviousStrikes < 18,
-						roll(index + 2) + if_(!isSpare(index + 2), roll(index + 1)))
-
-		def rollScoreAt(roll: Option[String]): (String, Int) =
-			roll match {
-				case Some("X") => (STRIKE._1, 10)
-				case Some("/") => (SPARE._1, 10)
-				case Some("-") => (MISS, 0)
-				case Some(_)   => (ROLL, roll.get.toInt)
-				case None => (NONE, 0)
+		def rollScoreAt(index: Int): Int =
+			rolls.lift(index) match {
+				case Some(STRIKE._1) => 10
+				case Some(SPARE._1)  => 10 - rolls(index - 1).toInt
+				case Some(MISS)      => 0
+				case Some(pins)      => pins.toInt
+				case None => 0
 			}
 
-		rollScoreAt(rolls.drop(index).headOption) match {
-			case (STRIKE._1, v) => totalScore(rolls, index + 1, score + strikeScore)
-			case (SPARE._1, v) => totalScore(rolls, index + 1, score + spareScore)
-			case (MISS, v) => totalScore(rolls, index + 1, score)
-			case (NONE, v) => score
-			case (ROLL, v) => totalScore(rolls, index + 1, score + v)
+		rolls.drop(index) match {
+			case List(STRIKE._1, _*) => totalScore(rolls, index + 1, score + STRIKE._2())
+			case List(SPARE._1, _*)  => totalScore(rolls, index + 1, score + SPARE._2())
+			case List(MISS, _*)      => totalScore(rolls, index + 1, score)
+			case List(n, _*)         => totalScore(rolls, index + 1, score + n.toInt)
+			case List()              => score
 		}
-
 	}
 
 	private def if_(condition: Boolean, ifTrue: => Int): Int = if (condition) ifTrue else 0
-
 }
